5f6a8a41a70f8193984defcef2d5606660e064b7
Aavrahman <aiguernazene@gmail.com>
2024-02-28 11:59:48 +0100

    Initial commit

---
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    // Chargement du style.css du thème parent Twenty Twenty
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // wp_enqueue_style('parent-style', get_stylesheet_uri());
+    // wp_enqueue_style('blanckslade-child-style', get_template_directory_uri() . '/style.css');
+
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    //  wp_enqueue_style('theme-style', get_parent_theme_file_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+
+/*
+    // Add an inline CSS
+    wp_add_inline_style(
+        'parent-style',
+        'body { background: lightblue; }
+    );          */
+}
+
+add_action('after_setup_theme', 'blankslate_child_setup');
+function blankslate_child_setup() {
+    add_editor_style(array(
+        get_stylesheet_uri(),
+        get_parent_theme_file_uri('css/theme.css')
+    ));
+}
